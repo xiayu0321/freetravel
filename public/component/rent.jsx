b0a915f347a011e0ce94c2a2f9cb667b33af570b
Leonard LiuTao <364125168@qq.com>
2016-08-22 18:57:08 +0800

    search tmp commit

--- public/component/rent.jsx
+++ public/component/rent.jsx
@@ -3,23 +3,46 @@ import request from 'superagent';
 
 import "../css/rent.css"
 class SelectArea extends React.Component {
+  constructor(props) {
+    super(props);
+    this.onSearch = this.props.onSearch;
+  }
   render() {
     return (
       <div className="select">
         <div className="row row-select">
           <div className="col-xs-2" id="search">搜索:</div>
           <div className="input-group text-search">
-            <input type="text" className="form-control" placeholder="请输入商品"/>
+            <input type="text" className="form-control" placeholder="请输入商品" onChange={this._searchItem.bind(this)}/>
             <span className="input-group-btn">
               <button className="btn btn-default btn-selete" type="button">搜索</button>
             </span>
+            <div></div>
           </div>
         </div>
         <div></div>
       </div>
     )
   }
+
+  _searchItem(event){
+    let keyword = event.target.value;
+    this.onSearch(keyword);
+  }
 }
+
+class SearchResult extends React.Component {
+  render() {
+    const {keyword, items}  = this.props;
+    return <div>
+      {
+        items.filter(item => item.toLowerCase().includes(keyword.toLowerCase()))
+          .map(i => <div>{i}</div>)
+      }
+    </div>;
+  }
+}
+
 class Picture extends React.Component {
   constructor(props) {
     super(props);
@@ -75,10 +98,19 @@ class Picture extends React.Component {
 
 
 class Rent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      keyword: ''
+    }
+  }
   render() {
     return (
       <div>
-        <SelectArea />
+        <SelectArea onSearch={(keyword) => this.setState({keyword: keyword})}/>
+        <SearchResult keyword={this.state.keyword} items={
+          ['JavaScript', 'Node', 'React', 'Redux', 'Bootstrap', 'Webpack', 'Babel']
+        }/>
         <Picture />
       </div>
     )
