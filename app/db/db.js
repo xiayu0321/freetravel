11252766bbb55d6fb01da8759342fdca371a47f5
xiayu0321 <xiayugogo0321@163.com>
2016-11-08 23:05:38 +0800

    bug

--- app/db/db.js
+++ app/db/db.js
@@ -2,11 +2,12 @@ var mongoose = require('mongoose');
 
 module.exports = {
   connect: function (mode, callback) {
-    let url = 'mongodb://localhost/freetravel-db';
+   let url = 'mongodb://localhost/freetravel-db';
+    //let url=process.env.PROD_MONGODB;
     if (mode === 'test') {
       url = 'mongodb://localhost/freetravel-test-db';
     }
-    mongoose.connect(url, callback);
+    mongoose.connect(url||process.env.PROD_MONGODB, callback);
   },
   close: function (callback) {
     mongoose.connection.close(callback);
